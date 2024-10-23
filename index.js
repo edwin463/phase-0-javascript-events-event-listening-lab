ba305e06fe491b57408bf29e5d8d01e918c59271
edwin karuri njoroge <edwin.njoroge1@student.moringaschool.com>
2024-10-23 09:43:51 -0700

    eventlistener

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+const input = document.querySelector('button');
+
+function addingEventListner(){
+    input.addEventListener('click', function(event){
+        return "clicked"
+        const input = document.getElementById("button");
+        input.addEventListener("click", function(){
+            alert("i was clicked!")
+        })
+    }
+
+)
 }
